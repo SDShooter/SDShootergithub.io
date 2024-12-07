dc3d29e7afddd50ed21fa9e4824dd634a231baa2
SDShooter <relicx74@gmail.com>
2024-12-06 16:59:17 -0800

    Sorting build system and splitting out /app vs /page router

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,60 @@
-/*ts-check*/
+// @ts-check
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
-
+// can convert to ->eslint.config.js worth anything?
+//https://typescript-eslint.io/getting-started/
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  "rules": {
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        "prefer": "type-imports",
+        "fixStyle": "inline-type-imports"
+      }
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        "argsIgnorePattern": "^_"
+      }
+    ],
+    "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      {
+        "checksVoidReturn": {
+          "attributes": false
+        }
+      }
+    ],
+    "drizzle/enforce-delete-with-where": [
+      "error",
+      {
+        "drizzleObjectName": [
+          "db",
+          "ctx.db"
+        ]
+      }
+    ],
+    "drizzle/enforce-update-with-where": [
+      "error",
+      {
+        "drizzleObjectName": [
+          "db",
+          "ctx.db"
+        ]
+      }
+    ]
+  }
+}
+//export default config
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,4 +63,11 @@ const compat = new FlatCompat({
     recommendedConfig: js.configs.recommended,
     allConfig: js.configs.all
 });
-export default [...compat.extends("next/core-web-vitals", "next/typescript")];
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+);
+
+
