025f9db057c76c76810dd23aab253cd1bdb6374b
SDShooter <relicx74@gmail.com>
2024-12-07 16:40:20 -0800

    explicity calling out gh-pages mjs file in type system

--- _deploy-gh-page.mjs
+++ _deploy-gh-page.mjs
@@ -1,11 +1,11 @@
 import ghpages from 'gh-pages';
-//var ghpages = require('gh-pages');
+//import ghtypes from '@types/gh-pages';
+
 ghpages.publish(
     'build',
     {
         branch: 'gh-pages',
         repo: 'https://github.com/SDShootergithub/SDShootergithub.github.io.git',
-        domain: 'https://bill.lowkeymg.com',
     },    
 ).then(() => {
     console.log('Deploy complete?');
